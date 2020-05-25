6ee17449ff35a915bc33d3e0d11d4d4e0f097a99
prfnv___ <pentalbymf@mail.ru>
2020-05-25 17:00:03 +0300

    task

--- labs/13/homework_mai.py
+++ labs/13/homework_mai.py
@@ -1,40 +1,36 @@
-import pandas as pd
-from sqlalchemy import create_engine
-
-base = 'localhost:5432/postgres'
-name_pass = 'login:password'
-pref = 'postgresql'
-engine = create_engine(f"{pref}://{name_pass}@{base}")
-
-
-def moving_average(d1: str, d2: str, window_size: int) -> pd.DataFrame:
+def moving_average(dateFrom: str, dateTo: str, window_size: int) -> pd.DataFrame:
     SQL = '''
-        SELECT
-            goods.g_group, sum(recgoods.volume * goods.length * goods.height * goods.width) s, recept.ddate
-        FROM goods 
-            JOIN recgoods on goods.id = recgoods.goods
-            JOIN recept on recept.id = recgoods.subid
-        WHERE 
-            recept.ddate >= %(mindate)s
-        AND 
-            recept.ddate <= %(maxdate)s
-        GROUP BY 
-            goods.g_group, recept.ddate
-        '''
+        SELECT goods.g_group goods_group,
+             recept.ddate date,
+             sum(recgoods.volume * goods.length * goods.height * goods.width) sum
+        FROM goods
+               JOIN recgoods ON goods.id = recgoods.goods
+               JOIN recept ON recept.id = recgoods.subid
+        WHERE recept.ddate >= %(mindate)s
+        AND recept.ddate <= %(maxdate)s
+        GROUP BY goods.g_group, recept.ddate
+    '''
 
-    df = pd.read_sql(SQL,
-                     engine,
-                     params={'mindate': d1, 'maxdate': d2},
-                     parse_dates={'recept.ddate': dict(format='%Y%m%d'), })
+    df = pd.read_sql(
+        SQL,
+        engine,
+        params={'mindate': dateFrom, 'maxdate': dateTo},
+        parse_dates={'recept.ddate': dict(format='%Y%m%d')}
+    )
 
     N = df.shape[0]
     if (N < window_size):
-        raise ValueError(f"Invalid windows size > {N}")
+        raise ValueError(f'Invalid windows size > {N}')
 
-    moving_average = df.s.rolling(window=window_size).mean()
-    names = ['goods', 'sum', 'recept']
+    dfs = df.set_index(['goods_group'])
+    dfs.drop('date', axis=1, inplace=True)
+    dfs['sum'] = dfs['sum'].shift(1)
+    dfs = dfs.groupby(level=['goods_group']).rolling(window=window_size).mean()
+    dfs.reset_index(level=[1], inplace=True)
+    dfs.reset_index(drop=True, inplace=True)
+    names = ['goods_group', 'date', 'sum']
     df.columns = names
-    df['prediction'] = moving_average
+    df['prediction'] = dfs['sum']
     df.to_sql('prediction', engine, if_exists='replace')
 
     return df
--- labs/7/task.sql
+++ labs/7/task.sql
@@ -1,63 +1,73 @@
-drop function if exists my_f(date, date, int);
-create or replace function my_f(d1 date, d2 date, window_size int)
-returns table (ggroup int, ddate date, ssum int, pred double precision) as $$
+drop function if exists moving_average(d1 date, d2 date);
+create or replace function moving_average(d1 date, d2 date)
+    returns table (goods_group int, date date, sum int, prediction double precision) as $$
 declare
-    curs cursor for select sum(recgoods.volume * goods.length * goods.height * goods.width),
-                            goods.g_group, recept.ddate
+    cursor cursor for select goods.g_group, recept.ddate,
+                             sum(recgoods.volume * goods.length * goods.height * goods.width)
         from goods
             join recgoods on goods.id = recgoods.goods
             join recept on recept.id = recgoods.subid
-        where recept.ddate >= '2019-01-02' and recept.ddate <= '2019-12-31'
+        where recept.ddate >= d1 and recept.ddate <= d2
         group by goods.g_group, recept.ddate;
 
-    N int := (select count(1)
-        from recept
-            join recgoods on (recept.id = recgoods.subid)
-        where recept.ddate >= d1 and recept.ddate <= d2
-        group by recept.client, recgoods.goods, recept.ddate);
-    pred double precision;
-    cnt  int;
---     cursor
-    gg   int;
-    dd   date;
-    ss   int;
+    cnt        int := 0;
+    temp_cnt   int := 0;
+    pred       double precision;
+    prev_group int;
+--
+    g_group    int;
+    date       date;
+    sum        int;
 
 
 begin
-    create temp table t (
-        ggroup int,
-        ddate  date,
-        ssum   int,
-        pred   double precision
+    create TEMP table tmp
+    (
+        goods_group int,
+        date        date,
+        sum         int
     );
-
-    if N < window_size then
-        insert into t values (Null, Null, Null, Null);
-        return query select * from t;
-        drop table t;
-    end if;
-    open curs;
-    cnt = 0;
+    create TEMP table to_return
+    (
+        goods_group int,
+        date        date,
+        sum         int,
+        prediction  double precision
+    );
+    open cursor;
     loop
-        fetch curs into gg, dd, ss;
+        fetch cursor into goods_group, date, sum;
         exit when not found;
-        if cnt < window_size then
-            insert into t values (gg, dd, ss, null);
-        else
-            pred = (select sum(t.ssum)
-                    from (select ssum, row_number() over () as row_n from t) as t
-                    where row_n >= cnt - window_size
-                      and row_n <= cnt) / window_size;
-            insert into t values (gg, dd, ss, pred);
-        end if;
-        cnt = cnt + 1;
 
+        if goods_group != prev_group then
+            temp_cnt := 1;
+            truncate tmp;
+            insert into tmp values (goods_group, date, sum);
+            insert into to_return (goods_group, date, sum, prediction)
+            values (goods_group, date, sum, sum);
+        else
+            if temp_cnt < 2 then
+                temp_cnt = temp_cnt + 1;
+                insert into to_return (goods_group, date, sum, prediction)
+                values (goods_group, date, sum, sum);
+                insert into tmp values (goods_group, date, sum);
+            else
+                temp_cnt = temp_cnt + 1;
+                insert into to_return (goods_group, date, sum, prediction)
+                values (goods_group, date, sum, (select avg(tmp.sum) from tmp));
+                delete from tmp where tmp.date in (select tmp.date from tmp order by tmp.date ASC limit 1);
+                insert into tmp values (goods_group, date, sum);
+            end if;
+        end if;
+        prev_group = goods_group;
+        cnt := cnt + 1;
     end loop;
 
-    close curs;
-    return query select * from t;
-    drop table t;
-end;
+    close cursor;
+    return query select * from to_return;
+    drop table to_return;
+    drop table tmp;
+end
 $$ language plpgsql;
 
-select * from my_f('2019-01-02', '2019-12-31', 2);
+select * from moving_average('2020-02-01', '2020-12-31');
